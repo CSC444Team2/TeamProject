91151aafda96674969292bc75d29fe78a32f97e8
Shirley <shirley@Grants-MacBook-Pro.local>
2016-10-05 12:33:06 -0400

    tournament controller works (index, new, edit, update, show)

--- tournament-app/app/controllers/tournaments_controller.rb
+++ tournament-app/app/controllers/tournaments_controller.rb
@@ -1,8 +1,36 @@
 class TournamentsController < ApplicationController
     def index
-            @tournaments = Tournament.all
+        @tournaments = Tournament.all
+    end
+    def show
+        @tournament = Tournament.find(params[:id])
+    end
+    def edit
+        @tournament = Tournament.find(params[:id])
+    end
+    def update
+        @tournament = Tournament.find(params[:id])
+        if @tournament.update_attributes(tournament_params)
+            redirect_to(:action => 'show', :id => @tournament.id)
+        else
+            render 'edit'
+        end
     end
     def new
-        
+        @tournament = Tournament.new
+    end
+    
+    def create
+        @tournament=Tournament.new(tournament_params)
+        if @tournament.save
+            redirect_to tournament_path(@tournament) #may change later '/tournaments'
+        else
+            render 'new'
+        end
+    end
+    
+    private
+    def tournament_params
+        params.require(:tournament).permit(:name, :id)
     end
 end
--- tournament-app/config/routes.rb
+++ tournament-app/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
     root 'tournaments#index'
+    #!!!! Do NOT use "form_for :blah", use "form_for(@blah)"!!!...
+    #post 'tournaments/new' => 'tournaments#create'
+    #post 'tournaments/:id/edit' => 'tournaments#create'
+    #!!!!
     resources :tournaments
+    
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
