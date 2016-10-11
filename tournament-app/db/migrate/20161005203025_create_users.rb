3e2534775dac987d46438fb4f5d16804ff192c54
Shirley <shirley@Grants-MacBook-Pro.local>
2016-10-11 13:35:53 -0400

    Bug fixes; always TEST befpre commit

--- tournament-app/db/migrate/20161005203025_create_users.rb
+++ tournament-app/db/migrate/20161005203025_create_users.rb
@@ -2,8 +2,9 @@ class CreateUsers < ActiveRecord::Migration
   def change
       # drop_table :users
     create_table :users do |t|
-      t.string :name
-      t.string :email
+      t.text :first_name #string
+      t.text :last_name
+      t.text :email
       # t.references :tournament, foreign_key: true
 
       t.timestamps
