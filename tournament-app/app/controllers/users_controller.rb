5c5a98c105ce65b9286d005af3ad0e876c053c7f
lzylizhengyu <zhengyu.li@mail.utoronto.ca>
2016-10-11 19:05:36 -0400

    fix passwd & passwd confirm function

--- tournament-app/app/controllers/users_controller.rb
+++ tournament-app/app/controllers/users_controller.rb
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
 	private
 		def user_params
-			params.require(:user).permit(:first_name, :last_name, :email, :password_digest)
+			params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
 		end
 end
