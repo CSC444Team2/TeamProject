7f6a7d482102bb0bd4217d970fcf875e94cce2d8
Shirley <shirley@Grants-MacBook-Pro.local>
2016-10-12 14:31:34 -0400

    home page recovered

--- tournament-app/config/routes.rb
+++ tournament-app/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
  	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  	root 'tournaments#index'
+  	root 'home#index'
 	#!!!! Do NOT use "form_for :blah", use "form_for(@blah)"!!!...
 	#post 'tournaments/new' => 'tournaments#create'
 	#post 'tournaments/:id/edit' => 'tournaments#create'
