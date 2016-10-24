bb23464655cbf85c000bb8576b40a3459304bdcb
Shirley Yang <myshirley.yang@mail.utoronto.ca>
2016-10-23 23:13:27 -0400

    bug fixes

--- tournament-app/app/controllers/users_controller.rb
+++ tournament-app/app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-	http_basic_authenticate_with name: "admin", password: "admin"#, except: [:index, :show]
+	http_basic_authenticate_with name: "admin", password: "admin", only: [:index]
 	#before_action :require_user, only: [:index, :show]
 	def index
 		@users = User.all
