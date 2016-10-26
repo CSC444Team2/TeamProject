3f1a6fea6b2c134e5ce787c5940ecb4e09ceb8fe
lzylizhengyu <zhengyu.li@mail.utoronto.ca>
2016-10-26 15:59:04 -0400

    Add location to tournaments

--- tournament-app/db/schema.rb
+++ tournament-app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161019131237) do
+ActiveRecord::Schema.define(version: 20161026195616) do
 
   create_table "organizes", force: :cascade do |t|
     t.integer  "event_id"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20161019131237) do
     t.text     "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "location"
   end
 
   create_table "users", force: :cascade do |t|
@@ -55,6 +56,8 @@ ActiveRecord::Schema.define(version: 20161019131237) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "reset_digest"
+    t.time     "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
