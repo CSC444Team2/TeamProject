d01c906cd6ae98b8328f9d839ba9e92650f96f66
Arcadius19 <arek.mikolajczak@mail.utoronto.ca>
2016-11-20 23:36:18 -0500

    Started working on PayPayl payments. For now "Sponsor" buttons just redirects to PayPal page with fixed data.

--- tournament-app/app/models/sponsor.rb
+++ tournament-app/app/models/sponsor.rb
@@ -4,4 +4,19 @@ class Sponsor < ApplicationRecord
 	
 	validates :person_id, presence: true
 	validates :event_id, presence: true
+
+	def paypal_url(return_path)
+		values = {
+				business: "csc444.toronto-merchant@gmail.com",
+				cmd: "_xclick",
+				upload: 1,
+				return: "www.google.com", #"#{Rails.application.secrets.app_host}#{return_path}",
+				invoice: id,
+				amount: 90,
+				item_name: sponsoring,
+				item_number: 2,
+				quantity: '1'
+		}
+		"#{Rails.application.secrets.paypal_host}/cgi-bin/webscr?" + values.to_query
+	end
 end
