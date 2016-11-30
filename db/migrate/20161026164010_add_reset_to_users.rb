0c76907ae6e5891136616163213e524b7159c045
Corey Yen <corey.yen@mail.utoronto.ca>
2016-11-30 18:03:09 -0500

    edited another migration

--- db/migrate/20161026164010_add_reset_to_users.rb
+++ db/migrate/20161026164010_add_reset_to_users.rb
@@ -1,6 +1,6 @@
 class AddResetToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :reset_digest, :string
-    add_column :users, :reset_sent_at, :datatime
+    add_column :users, :reset_sent_at, :datetime
   end
 end
