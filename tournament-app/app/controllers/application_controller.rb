ef112925d543849e4640e4cb5226834fee7366b8
regan1219 <mingwei.ding@mail.utoronto.ca>
2016-11-29 22:40:42 -0500

    Update application_controller.rb
    def current_user

--- tournament-app/app/controllers/application_controller.rb
+++ tournament-app/app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+  def current_user
+  @current_user ||= User.find_by_auth_token!(cookies[:auth_token]) if cookies[:auth_token]
+  end
 end
 
 
