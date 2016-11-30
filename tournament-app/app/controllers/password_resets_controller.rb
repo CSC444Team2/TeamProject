86963607cf9e335f1902f5f2776d3b80d6a05777
regan1219 <mingwei.ding@mail.utoronto.ca>
2016-11-29 22:53:06 -0500

    Update password_resets_controller.rb
    new version

--- tournament-app/app/controllers/password_resets_controller.rb
+++ tournament-app/app/controllers/password_resets_controller.rb
@@ -1,65 +1,32 @@
 class PasswordResetsController < ApplicationController
-  before_action :get_user,         only: [:edit, :update]
-  before_action :valid_user,       only: [:edit, :update]
-  before_action :check_expiration, only: [:edit, :update]    # Case (1)
-
-  def new
-  end
-
-  def create
-    @user = User.find_by(email: params[:password_reset][:email].downcase)
-    if @user
-      @user.create_reset_digest
-      @user.send_password_reset_email
-      flash[:info] = "Email sent with password reset instructions"
-      redirect_to root_url
-    else
-      flash.now[:danger] = "Email address not found"
+ def new
+ end
+ 
+ def create
+  @user = User.find_by_email(params[:email])
+  if @user
+  @user.send_password_reset
+  redirect_to root_url, :notice => "Email sent with password reset instructions."
+  else
+     flash.now[:danger] = "Email address not found"
       render 'new'
     end
-  end
-
-  def edit
-  end
-
-  def update
-    if params[:user][:password].empty?                  # Case (3)
-      @user.errors.add(:password, "can't be empty")
-      render 'edit'
-    elsif @user.update_attributes(user_params)          # Case (4)
-      log_in @user
-      flash[:success] = "Password has been reset."
-      redirect_to @user
-    else
-      render 'edit'                                     # Case (2)
-    end
-  end
-
-  private
-
-    def user_params
-      params.require(:user).permit(:password, :password_confirmation)
-    end
-
-    # Before filters
-
-    def get_user
-      @user = User.find_by(email: params[:email])
-    end
-
-    # Confirms a valid user.
-    def valid_user
-      unless (@user && @user.activated? &&
-              @user.authenticated?(:reset, params[:id]))
-        redirect_to root_url
-      end
-    end
-
-    # Checks expiration of reset token.
-    def check_expiration
-      if @user.password_reset_expired?
-        flash[:danger] = "Password reset has expired."
-        redirect_to new_password_reset_url
-      end
-    end
 end
+
+def edit
+  @user = User.find_by_password_reset_token!(params[:id])
+end
+
+def update
+  @user = User.find_by_password_reset_token!(params[:id])
+  if @user.password_reset_sent_at < 2.hours.ago
+    redirect_to new_password_reset_path, :alert => "Password reset has expired."
+  elsif @user.update_attribute('password', params[:user][:password])
+    redirect_to root_url, :notice => "Password has been reset!"
+  else
+    render :edit
+  end
+end
+end
+
+   
