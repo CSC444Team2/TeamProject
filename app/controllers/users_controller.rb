94542dcf29ee80837e7ca9213ae3689dc4964c51
Corey Yen <corey.yen@mail.utoronto.ca>
2016-12-04 16:02:50 -0500

    fixed some typos, allow display of addresses and phone number in profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,7 +71,7 @@ class UsersController < ApplicationController
 
 	private
 		def user_params
-			params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+			params.require(:user).permit(:first_name, :last_name, :gender, :date_of_birth, :phone_number, :address, :email, :password, :password_confirmation)
 		end
 		def is_user_profile
 			@user = User.find(params[:id])
