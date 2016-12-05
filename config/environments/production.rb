30f9a7a6f1fed9905aeb180595c3924b3d681418
Corey Yen <corey.yen@mail.utoronto.ca>
2016-12-05 16:15:33 -0500

    more fixes for s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,24 +82,15 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  # config.active_record.dump_schema_after_migration = false
-  #config.paperclip_defaults = {
-  #storage: :s3,
-  #s3_credentials: {
-    #bucket: ENV.fetch('S3_BUCKET_NAME'),
-    #access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    #secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    #s3_region: ENV.fetch('AWS_REGION'),
-    #}
- # }
+  config.active_record.dump_schema_after_migration = false
   config.paperclip_defaults = {
-    :storage => :s3,
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :s3_credentials => {
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    },
-    :s3_region => ENV['AWS_REGION']
+  storage: :s3,
+  s3_region: ENV.fetch('AWS_REGION'),
+  s3_credentials: {
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),   
+    }
   }
-
+  
 end
