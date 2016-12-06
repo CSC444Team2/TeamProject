2483067d560ab5afd7f4644a0270e9b3252cb57f
Corey Yen <corey.yen@mail.utoronto.ca>
2016-12-06 14:40:16 -0500

    oops

--- db/migrate/20161206192941_remove_is_private_from_tournament.rb
+++
@@ -1,6 +0,0 @@
-class RemoveIsPrivateFromTournament < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :tournaments, :is_private, :integer
-    add_column :tournaments, :is_private, :integer
-  end
-end
