59a48c69768a9fa57c85eebf9757b4cba6e3cd81
shirleyy-yang <myshirley.yang@mail.utoronto.ca>
2016-12-06 21:13:33 -0500

    golf request controller in progress

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,12 +1,28 @@
 class RequestsController < ApplicationController
 	def new
-		@request=Request.new
+		
 		if current_user.nil?
 			redirect_to login_path
 		else
 			@is_golf_course = params[:is_golf_course]
 			if !@is_golf_course.nil?
+				@golf_request_type = params[:golf_request_type]
+				@request=GolfRequest.new(golf_request_type: @golf_request_type)
+				@candidate_courses = GolfCourse.all
+				@candidate_courses = @candidate_courses.reject do |g|
+					if @golf_request_type == 0
+						g.got_admin?(current_user)
+					elsif @golf_request_type == 1
+						g.got_csr?(current_user)
+					end
+				end
+
+				@selected_course = params[:selected_course]
+				if @selected_course.nil? && !@candidate_courses.empty?
+					@selected_course = 1
+				end
 			else
+				@request=Request.new
 				@candidate_events = Tournament.all
 				#Do not include events that the user already organizes
 				@candidate_events = @candidate_events.reject do |t|
@@ -17,6 +33,7 @@ class RequestsController < ApplicationController
 				@selected_event=params[:selected_event]
 				if @selected_event.nil? && !@candidate_events.empty?
 					@selected_event=1
+				end
 			end
 		end
 	end
