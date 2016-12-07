4726ae1717468f31c4e86d2cc11162da3fb4b5f2
Corey Yen <corey.yen@mail.utoronto.ca>
2016-12-06 19:45:40 -0500

    JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery_ujs
-//= require jquery
 //= require bootstrap
+//= require jquery
+//= require jquery_ujs
 //= require turbolinks
 //= require_tree .
